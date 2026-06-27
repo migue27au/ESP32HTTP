195c51f9d802d3c0c4f634f92ea3b8bf21fe5cd3
migue27au <43569365+migue27au@users.noreply.github.com>
2026-06-27 22:11:29 +0200

    Update ESP32HTTP.cpp

--- ESP32HTTP/ESP32HTTP.cpp
+++ ESP32HTTP/ESP32HTTP.cpp
@@ -5,23 +5,23 @@ int utilCharToInt(char c){
   switch(c){
     case '0':
       return 0;
-    case'1':
+    case '1':
       return 1;
-    case'2':
+    case '2':
       return 2;
-    case'3':
+    case '3':
       return 3;
-    case'4':
+    case '4':
       return 4;
-    case'5':
+    case '5':
       return 5;
-    case'6':
+    case '6':
       return 6;
-    case'7':
+    case '7':
       return 7;
-    case'8':
+    case '8':
       return 8;
-    case'9':
+    case '9':
       return 9;
     default:
       return -1;
